76d063982e3de9b5c03cf2cd945d16d22f41c6d1
Agus Awaludin <55693286+AgusAwaludinUnikom@users.noreply.github.com>
2019-09-23 18:37:50 +0700

    Add files via upload

---
+++ src/if2/pkg10118067/latihan10/IF210118067Latihan10.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118067.latihan10;
+
+/**
+ *
+ * @author
+ * Nama     : Agus Awaludin
+ * Kelas    : IF-2
+ * NIM      : 10118067
+ * Latihan 10. Logika
+ */
+public class IF210118067Latihan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=17);
+        System.out.println("Status Dewasa : " +dewasa);
+    }
+    
+}
